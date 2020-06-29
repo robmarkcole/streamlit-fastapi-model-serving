23eae75964b7549a542f86b53d3270a49eaeaf6e
Robin Cole <robmarkcole@gmail.com>
2020-06-29 05:29:25 +0100

    Add cache predict

--- fastapi/server.py
+++ fastapi/server.py
@@ -5,17 +5,20 @@ from segmentation import get_segmentator, get_segments
 
 model = get_segmentator()
 
-app = FastAPI(title="DeepLabV3 image segmentation",
-              description='''Obtain semantic segmentation maps of the image in input via DeepLabV3 implemented in PyTorch.
-                           Visit this URL at port 8501 for the streamlit interface.''',
-              version="0.1.0",
-              )
+app = FastAPI(
+    title="DeepLabV3 image segmentation",
+    description="""Obtain semantic segmentation maps of the image in input via DeepLabV3 implemented in PyTorch.
+                           Visit this URL at port 8501 for the streamlit interface.""",
+    version="0.1.0",
+)
 
 
 @app.post("/segmentation")
 async def get_segmentation_map(file: bytes = File(...)):
-    '''Get segmentation maps from image file'''
+    """Get segmentation maps from image file"""
     segmented_image = get_segments(model, file)
-    with tempfile.NamedTemporaryFile(mode="w+b", suffix=".png", delete=False) as outfile:
+    with tempfile.NamedTemporaryFile(
+        mode="w+b", suffix=".png", delete=False
+    ) as outfile:
         segmented_image.save(outfile)
         return FileResponse(outfile.name, media_type="image/png")
--- streamlit/ui.py
+++ streamlit/ui.py
@@ -4,34 +4,34 @@ import requests
 from PIL import Image
 import io
 
-st.title('DeepLabV3 image segmentation')
+st.title("DeepLabV3 image segmentation")
 
 # fastapi endpoint
-url = 'http://fastapi:8000'
-endpoint = '/segmentation'
+url = "http://fastapi:8000"
+endpoint = "/segmentation"
 
-st.write('''Obtain semantic segmentation maps of the image in input via DeepLabV3 implemented in PyTorch.
+st.write(
+    """Obtain semantic segmentation maps of the image in input via DeepLabV3 implemented in PyTorch.
          This streamlit example uses a FastAPI service as backend.
-         Visit this URL at `:8000/docs` for FastAPI documentation.''') # description and instructions
+         Visit this URL at `:8000/docs` for FastAPI documentation."""
+)  # description and instructions
 
-image = st.file_uploader('insert image')  # image upload widget
+image = st.file_uploader("insert image")  # image upload widget
 
 
+@st.cache
 def process(image, server_url: str):
-
-    m = MultipartEncoder(
-        fields={'file': ('filename', image, 'image/jpeg')}
-        )
-
-    r = requests.post(server_url,
-                      data=m,
-                      headers={'Content-Type': m.content_type},
-                      timeout=8000)
-
+    m = MultipartEncoder(fields={"file": ("filename", image, "image/jpeg")})
+    r = requests.post(
+        server_url,
+        data=m,
+        headers={"Content-Type": m.content_type},
+        timeout=8000,
+    )
     return r
 
 
-if st.button('Get segmentation map'):
-    segments = process(image, url+endpoint)
-    segmented_image = Image.open(io.BytesIO(segments.content)).convert('RGB')
-    st.image([image, segmented_image], width=300) # output dyptich
+if st.button("Get segmentation map"):
+    segments = process(image, url + endpoint)
+    segmented_image = Image.open(io.BytesIO(segments.content)).convert("RGB")
+    st.image([image, segmented_image], width=300)  # output dyptich
